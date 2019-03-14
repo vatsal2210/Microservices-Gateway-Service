188d5015387b0f9f50776e85519dcc3e21729c02
Vatsal Shah <vshah56@uwo.ca>
2019-03-14 01:54:18 -0400

    changes

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,5 @@
-const jwt = require("jsonwebtoken");
-const config = require("../config/config");
+const axios = require('axios');
+const authURL = "192.168.0.23:8080/user/verify";
 
 module.exports = (req, res, next) => {
     const authHeader = req.get("Authorization");
@@ -9,19 +9,21 @@ module.exports = (req, res, next) => {
         throw error;
     }
     const token = authHeader.split(" ")[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, config.JWT.JWT_ENCRYPTION);
-    } catch (error) {
-        error.status = 500;
-        throw error;
-    }
-    if (!decodedToken) {
-        const error = new Error("Not authenticated.");
-        error.status = 401;
-        throw error;
-    }
-    //console.log("decodedToken ", decodedToken);
-    req.userId = decodedToken.userId;
-    next();
+
+    axios.post(authURL, {
+            token: token
+        })
+        .then(function (data) {
+            // handle success
+            console.log('authenticated req', data);
+            next();
+        })
+        .catch(function (error) {
+            // handle error
+            console.log('error', error);
+            return res.send({
+                code: 401,
+                message: "Not authenticated."
+            });
+        })
 };
--- routers/service.js
+++ routers/service.js
@@ -13,7 +13,8 @@ router.get('/testing', (req, res) => {
     res.send('Gateway working');
 });
 
-router.get("/list", serviceController.list);
+router.get("/list",  serviceController.list);
 router.post("/search", searchController.search);
+router.post("/search1", isAuth, searchController.search);
 
 module.exports = router;
