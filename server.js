0902810630f040e91f194363c827c9c4fcfc9ee4
Vatsal Shah <vshah56@uwo.ca>
2019-04-04 02:45:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,15 +36,6 @@ app.use(
 
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(
   bodyParser.json({
     limit: 5242880
